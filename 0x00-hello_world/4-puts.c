35b6af5e941d80d71ce6d7ffb5456e9df9627c0b
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-09-08 16:51:06 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
- 
- *fuction used: puts
- 
+ *fuction used: puts 
  *printf not allowed
-
  *return 0
  */
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-
-    return(0);
-
+  return(0);
 }
