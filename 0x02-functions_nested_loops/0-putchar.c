3e8287d8a999ea0c17ca3da3b17dcc6125fd3d00
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-09-13 14:57:45 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-char putf[] = "_putchar";
+char abe[] = "_putchar";
 int c;
 for (c = 0; c < 8; c++)
 {
-_putchar(putf[c]);
+_putchar(abe[c]);
 }
 _putchar('\n');
 return (0);
