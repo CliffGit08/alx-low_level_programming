f972fad651ef5fa49c39dac6326d90cb504b65e9
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-09-19 12:25:51 -0700

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,15 @@
 * Return: Nothing
 */
 
-void print_rev(char *s)
+void print_rev(char *s) 
 {
-int index;
-for (index = 0; s[index] != '\0'; ++index);
+  int index;
 
-for (--index; index >= 0; --index)
-_putchar(s[index]);
-_putchar('\n');
+  /*finds the length of string without null character*/
+  for (index = 0; s[index] != '\0'; ++index);
+  
+  /*print char from the last index as you decrement*/
+  for (--index; index >= 0; --index)
+    _putchar(s[index]);
+  _putchar('\n'); 
 }
