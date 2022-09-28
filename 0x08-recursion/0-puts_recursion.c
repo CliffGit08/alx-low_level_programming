13f182bd1f4dda59287ec90d53fe6bb1e7231c55
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-09-28 07:14:08 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - puts a string to stdout in reverse
- *
- * @s: string to put to stdout
- *
- * Return: always voi
-*/
-
-void _print_rev_recursion(char *s)
-
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s)
 {
-if (!*s) /* if s points to null char */
-return;
-_print_rev_recursion(s + 1);
-_putchar(*s);
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
