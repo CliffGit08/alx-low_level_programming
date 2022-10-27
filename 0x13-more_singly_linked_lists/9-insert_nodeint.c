8794c33d1192c821d71a5234c1851e79d5d48f70
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2022-10-26 17:22:49 -0700

    meticulus

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,45 @@
 #include "lists.h"
+
 /**
-  * insert_nodeint_at_index - inserts a new node at a given position.
-  * @head: pointer to pointer of the first element on list.
-  * @idx: index of list where the new node should be added.
-  * @n: integer to be inserted.
-  *
-  * Return: address of the new node or NULL if it fails.
-  */
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp;
-	unsigned int count;
+	listint_t *new, *copy = *head;
+	unsigned int node;
 
-	temp = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-	count = 0;
-	while (temp && count < idx - 1)
+	new->n = n;
+
+	if (idx == 0)
 	{
-		temp = temp->next;
-		count++;
+		new->next = copy;
+		*head = new;
+		return (new);
 	}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node != NULL)
+	for (node = 0; node < (idx - 1); node++)
 	{
-		new_node->n = n;
-		if (idx == 0)
-		{
-			new_node->next = *head;
-			*head = new_node;
-			return (new_node);
-		}
-		if (count + 1 == idx)
-		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
-		}
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
 	}
-	free(new_node);
-	return (NULL);
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
 }
